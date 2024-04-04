f333dd907c2c7a854bc9755f58478d7a3c7ae658
devraj singh tomar <devrajsingh.t11@gmail.com>
2024-04-04 22:24:20 +0530

    sliding text done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Landingpage from './components/Landingpage';
+import Slidingtext from './components/Slidingtext'
 
 const App = () => {
   return (
-   <div className='w-full h-screen bg-zinc-900 text-white'>
+   <div className='w-full min-h-screen bg-zinc-900 text-white'>
 
       <Navbar />
       <Landingpage />
+      <Slidingtext/>
     </div>
   )
 }
